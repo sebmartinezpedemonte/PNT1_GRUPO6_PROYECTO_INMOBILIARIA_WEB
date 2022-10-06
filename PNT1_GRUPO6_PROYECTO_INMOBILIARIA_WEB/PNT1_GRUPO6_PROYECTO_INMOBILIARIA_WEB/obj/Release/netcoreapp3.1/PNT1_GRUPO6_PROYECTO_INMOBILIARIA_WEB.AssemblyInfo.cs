9f329cf0ae6923039836a6d80e26b53444f9ca3e
Felix Lamas <felix.lamas@hotmail.com>
2022-10-05 22:10:00 -0300

    español

--- PNT1_GRUPO6_PROYECTO_INMOBILIARIA_WEB/PNT1_GRUPO6_PROYECTO_INMOBILIARIA_WEB/obj/Release/netcoreapp3.1/PNT1_GRUPO6_PROYECTO_INMOBILIARIA_WEB.AssemblyInfo.cs
+++ PNT1_GRUPO6_PROYECTO_INMOBILIARIA_WEB/PNT1_GRUPO6_PROYECTO_INMOBILIARIA_WEB/obj/Release/netcoreapp3.1/PNT1_GRUPO6_PROYECTO_INMOBILIARIA_WEB.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PNT1_GRUPO6_PROYECTO_INMOBILIARIA_WEB")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PNT1_GRUPO6_PROYECTO_INMOBILIARIA_WEB")]
